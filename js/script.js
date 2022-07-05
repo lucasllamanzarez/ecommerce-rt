e51eaeeba127969e9ce1d7ea96cf8234b6aa9d2f
Lucas Llamanzarez <lucas.llamanzarez@gmail.com>
2022-07-05 19:13:49 -0300

    Desafio Reentrega 30/06 - Ecommerce

--- js/script.js
+++ js/script.js
@@ -1,31 +1,56 @@
-//Llamo archivo JSON para cargar los productos en el localStorage
-fetch("json/productos.json")
-.then(response => {
-   return response.json();
-})
-.then(jsonData => localStorage.setItem('producto', JSON.stringify(jsonData)));
+//import(productos);
+//Array de Productos
+var productos = [
+   {
+       "id": 1,
+       "nombre": "Teclado Logitech",
+       "precio": 10000,
+       "imagen": "./images/teclado.jpg"
+   },
+    {
+       "id": 2,
+       "nombre": "Mouse Logitech",
+       "precio": 5000,
+       "imagen": "./images/mouse.jpg"
+   }, {
+       "id": 3,
+       "nombre": "Gabinete Thermaltake",
+       "precio": 8000,
+       "imagen": "./images/gabiente.jpg"
+   }, {
+       "id": 4,
+       "nombre": "Fuente Thermaltake",
+       "precio": 12000,
+       "imagen": "./images/fuente.jpg"
+   }, {
+       "id": 5,
+       "nombre": "Placa de video Asus",
+       "precio": 75000,
+       "imagen": "./images/placavideo.jpg"
+   }
+   ];
 
 
-//Muestro lo productos cargados previamente en el LocalStorage
-const listaProd = localStorage.getItem('producto');
-//console.log('listaProductos: ', JSON.parse(listaProd));
-
-
-
-//Escribo Productos en el DIV
+//Recorro el array
+   for (i = 0; i < productos.length; i++) {
+      
+    
+//Escribo Productos en el DIV del HTML
 const divProducto = document.createElement("div");
 
-const nombreProducto = document.createElement("h3");
-         nombreProducto.textContent = `Nombre producto: ` + listaProd;
-            divProducto.appendChild(nombreProducto);
+   const nombreProducto = document.createElement("h3");
+         nombreProducto.textContent = `Producto: ` + productos[i].nombre;
+               divProducto.appendChild(nombreProducto);
             
+   const precioProducto = document.createElement("p");
+         precioProducto.textContent = `Precio: $` + productos[i].precio;
+               divProducto.appendChild(precioProducto);
 
-const precioProducto = document.createElement("p");
-         precioProducto.textContent = `Precio producto: $` + listaProd;
-            divProducto.appendChild(precioProducto);
+   const imgProducto = document.createElement("img");
+                                          imgProducto.src = productos[i].imagen;
+               divProducto.appendChild(imgProducto);
+   const btnProducto = document.createElement("button");
 
 document.getElementById("divProd").appendChild(divProducto);
 
-
-
-
+} 
